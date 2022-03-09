7c899361abb3a74b39e859a068bcb645108387d0
Wan Muzaffar <wanmuz86@gmail.com>
2022-03-09 11:21:24 +0800

    add item

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
     //
 
     public function create(Request $request){
+
+        $movie = new Movie();
+        $movie->title = $request->title;
+        $movie->poster_url = $request->poster_url;
+        $movie->synopsis = $request->synopsis;
+        $movie->video_url = $request->video_url;
+        $movie->director = 1;
+        $movie->category = 1;
+        if ($movie->save()){
+            return response()->json(["success"=>true, "data"=>movie]);
+        }
 
     }
 
@@ -19,7 +31,7 @@ class MovieController extends Controller
     public function getById(Request $request, $id){
 
     }
-    
+
     public function update(Request $request, $id){
 
     }
