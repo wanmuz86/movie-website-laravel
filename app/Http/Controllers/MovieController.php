f2b7e462af2712590c73a325d65fff53c606bc94
Wan Muzaffar <wanmuz86@gmail.com>
2022-03-09 11:14:40 +0800

    add item

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,4 +7,24 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     //
+
+    public function create(Request $request){
+
+    }
+
+    public function getAll(Request $request){
+
+    }
+
+    public function getById(Request $request, $id){
+
+    }
+    
+    public function update(Request $request, $id){
+
+    }
+
+    public function delete(Request $request, $id){
+
+    }
 }
